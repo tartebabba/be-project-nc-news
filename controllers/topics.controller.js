3ccf1db4f66d62380056aa0f3aad563bbdbab8e8
tartebabba <jez.piedad@gmail>
2024-04-15 14:07:49 +0100

    added an endpoint to view endpoints.json, with tests

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,4 +1,15 @@
-const { fetchAllTopics } = require('../models/topics.models');
+const {
+  fetchAllTopics,
+  fetchEndpointsDescription,
+} = require('../models/topics.models');
+
+exports.getEndpointsDescription = (req, res, next) => {
+  return fetchEndpointsDescription()
+    .then((endpoints) => {
+      res.status(200).send(endpoints);
+    })
+    .catch((err) => next(err));
+};
 
 exports.getTopics = (req, res, next) => {
   return fetchAllTopics()
