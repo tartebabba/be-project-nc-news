f596ac87a3eed07a1699754e923f561e218c5fd3
tartebabba <jez.piedad@gmail>
2024-04-15 15:18:39 +0100

    added get Article by ID functionality, with tests

--- __tests__/integrations.test.js
+++ __tests__/integrations.test.js
@@ -61,3 +61,46 @@ describe('Topics', () => {
   });
   test.todo('Test for no topics found..?');
 });
+
+// ARTICLES
+describe.only('Articles', () => {
+  test('GET 200: Endpoint returns an a singular article by ID, with the appropriate properties.', () => {
+    return request(app)
+      .get('/api/articles/1')
+      .expect(200)
+      .then(({ body }) => {
+        const { article } = body
+        const expectedArticle = {
+          article_id: 1,
+          title: 'Living in the shadow of a great man',
+          topic: 'mitch',
+          author: 'butter_bridge',
+          body: 'I find this existence challenging',
+          created_at: '2020-07-09T20:11:00.000Z',
+          votes: 100,
+          article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700'
+        }
+        const expectedKeys = ["article_id", "title", "topic", "author", "body", "created_at", "votes", "article_img_url"];
+        expectedKeys.forEach((key) => {
+          expect(key in article).toBe(true)
+        })
+        expect(Object.keys(article).length).toBe(8)
+        expect(article).toMatchObject(expectedArticle);
+        expect(typeof article.article_id).toBe("number");
+        expect(typeof article.title).toBe("string");
+        expect(typeof article.topic).toBe("string");
+        expect(typeof article.author).toBe("string");
+        expect(typeof article.body).toBe("string");
+        expect(typeof article.created_at).toBe("string")
+        expect(typeof article.votes).toBe("number");
+        expect(typeof article.article_img_url).toBe("string");
+      });
+  });
+  test.todo('Test for no topics found..?');
+});
+
+// [] Update fetch-topics to send back with meaningful key.
+// [] Assert the types for each of the keys for fetch-topics.
+// [] Update the error message to use toMatchObjects().
+// [] Remove all console.logs.
+// [] Update endpoints test to only require the file and not go through fs
