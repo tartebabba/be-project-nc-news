2bd39c13c5e4131f6d4aa9429fef929bad3a4d95
tartebabba <jez.piedad@gmail>
2024-04-15 16:18:40 +0100

    Update articles.models.js

--- models/articles.models.js
+++ models/articles.models.js
@@ -6,7 +6,6 @@ const recordNotFound = "Sorry! That particular record was not found"
 exports.fetchArticleByID = (articleID) => {
   const articleByIdQuery = `SELECT article_id, title, topic, author, body, created_at, votes, article_img_url FROM articles
   WHERE article_id = $1;`;
-  // console.log(articleID);
   return db.query(articleByIdQuery, [articleID]).then(({ rows }) => {
     if (!rows.length)
       return Promise.reject({ status: 404, errorMessage: recordNotFound });
